af2ecb45333f954531f78161903a70122cf251ad
shaochen Ren <ren06060512@gmail.com.com>
2023-07-09 01:08:16 -0400

    the unseen navbar bug fixed

--- src/components/topnav/Topnav.jsx
+++ src/components/topnav/Topnav.jsx
@@ -7,23 +7,28 @@ const Navbar = () => {
   const [show, setShow] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 100) { // Adjust this value as needed
-        setShow(true);
-      } else {
-        setShow(false);
-      }
-    });
-    return () => {
-      window.removeEventListener('scroll');
+    const checkScroll = () => {
+        if (window.scrollY > 0) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
     };
-  }, []);
+
+    window.addEventListener('scroll', checkScroll);
+    
+    return () => {
+        window.removeEventListener('scroll', checkScroll);
+    };
+}, []);
 
   return (
     <nav className={`navbar ${show && 'navbar__show'}`}>
       <a href="/home" className="navbar__link">Home</a>
       <a href="/about" className="navbar__link">About</a>
       <a href="/contact" className="navbar__link">Contact</a>
+      <a href="/Recent-life" className="navbar__link">Recent-life</a>
+      <a href="/Blogs" className="navbar__link">Blogs</a>
     </nav>
   );
 }
