f47d48780fa51733d531610806fefb933e384f9a
shaochen Ren <ren06060512@gmail.com.com>
2023-07-14 22:55:09 -0400

    partial updated

--- back-end/myproject/myapp/models.py
+++ back-end/myproject/myapp/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Image(models.Model):
     url = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    text = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
